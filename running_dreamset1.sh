3539b12f2617d6887baae4f5443afb1a40da5122
sstadick <sstadick@gmail.com>
2016-05-07 06:31:14 -0400

    Cleanup

--- running_dreamset1.sh
+++
@@ -1,8 +0,0 @@
-# bowtie2
-/home/ubuntu/sstadick/scripts/varsim/run_pipeline.py -o /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/bowtieDream1 / -b /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/bowtieDream1/BEDS/ROI.bed -a bowtie2 -c mutect -s align -t bowtieDream1 -r real -e /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/synthetic.challenge.set1.tumor.v2.bam
-
-#bwa
-/home/ubuntu/sstadick/scripts/varsim/run_pipeline.py -o /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/bwaDream1 / -b /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/bwaDream1/BEDS/ROI.bed -a bwa -c mutect -s align -t bwaDream1 -r real -e /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/synthetic.challenge.set1.tumor.v2.bam
-
-#novoalign
-/home/ubuntu/sstadick/scripts/varsim/run_pipeline.py -o /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/novoDream1 / -b /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/novoDream1/BEDS/ROI.bed -a bwa -c mutect -s align -t novoDream1 -r real -e /mnt2/DREAM/06683320-dbd9-464e-b6f2-143e715b2981/synthetic.challenge.set1.tumor.v2.bam
--- scripts/metarun.sh
+++
@@ -1,4 +0,0 @@
-export PATH=$PATH:/home/ubuntu/jrw_pgdx/jwhite-bin/
-#bash /mnt2/SIM/hg19_vaf_20/create_reads.sh > /mnt2/SIM/hg19_vaf_20/reads.log 2>1&
-bash /mnt2/SIM/hg19_vaf_20/add_mutations.sh > /mnt2/SIM/hg19_vaf_20/muts.log 2>1&
-bash /mnt2/SIM/hg19_vaf_20/vaf_aln.sh > /mnt2/SIM/hg19_vaf_20/aln.log 2>1&
